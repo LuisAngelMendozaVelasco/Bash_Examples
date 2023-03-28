7541dd9612da187303f3baaf1d4c88e477897d37
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-28 16:39:45 -0600

    Create Example_2.sh

---
+++ Example_2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Echo commands
+echo “printing text with newline”
+echo -n “printing text without newline” # ‘-n’ to print any text without a new line
+echo -e “\nRemoving \t backslash \t characters\" # ‘-e’ command to remove backslash characters
