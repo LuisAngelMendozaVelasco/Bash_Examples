af63624e0d9f20e289fc533ae0e4b5a145202a80
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-29 00:11:50 -0600

    Create Example_31.sh

---
+++ Example_31.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# The AND Operator
+echo -n "Input a number less than 20: "
+read num
+
+if [[ ( $num -lt 20 ) && ( $num%2 -eq 0 ) ]] # -lt->less than, -eq->equal
+then
+    echo "It is an even number!"
+else
+    echo "It is an odd number!"
+fi
