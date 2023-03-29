674ca07634fc8bdd7b66f2adad8e3d4b658e26c3
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-28 19:49:31 -0600

    Create Example_13.sh

---
+++ Example_13.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Obtaining arguments from Command Line
+
+# Variables $1 and $3 are used to cypher through the first and third command-line arguments, respectively
+echo "Total arguments: $#"
+echo "1st Argument = $1"
+echo "3rd argument = $3"
