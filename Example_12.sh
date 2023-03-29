40d3572b61c369e838f88e0c13f5ab5f83b2f3e7
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-28 18:48:39 -0600

    Create Example_12.sh

---
+++ Example_12.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Case statement
+echo -n "Enter your lucky number: " # ‘-n’ to print any text without a new line
+read s
+
+case $s in
+    50)
+        echo echo "You won the 1st bravo!!!!" ;;
+    100)
+        echo "You won the 2nd congrats!!!!" ;;
+    500)
+        echo "You won the 3rd congrats" ;;
+    *)
+        echo "Sorry, you have to keep trying pal!" ;;
+esac
