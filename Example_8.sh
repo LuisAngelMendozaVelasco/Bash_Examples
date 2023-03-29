e722c96e6133d872393bbfb2f21eb897ecfb7c35
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-28 18:37:15 -0600

    Update Example_8.sh

--- Example_8.sh
+++ Example_8.sh
@@ -4,7 +4,7 @@ echo -n "Enter an integer number: " # ‘-n’ to print any text without a new l
 read s
 if (($s%2 == 0))
 then
-    echo "It is divisible by 2"
+    echo "It is divisible by 2!"
 else
-    echo "It is not divisible by 2"
+    echo "It is not divisible by 2!"
 fi
