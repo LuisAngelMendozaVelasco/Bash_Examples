4d9dd8b5f73a51f88cc722aa30a0c2215eaee2a7
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-03-28 23:47:41 -0600

    Create Example_27.sh

---
+++ Example_27.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Send Email
+Recipient="linux@example.com"
+Subject="inquiries"
+Message="Need anything from linux?"
+# <<< passes the word on the right to the standard input of the command on the left.
+`mail -s $Subject $Recipient <<< $Message` # ‘-s’ option is used in the `mail` command to define the subject of the email.
